a717087efd36f8951ca4d9eb3c734df8529e1db6
xebi <kabuotoozebdiel>
2026-05-09 23:29:09 -1200

    still working on responsiveness

--- crab-boutique/src/COMPONENTS/Home.jsx
+++ crab-boutique/src/COMPONENTS/Home.jsx
@@ -77,6 +77,21 @@ function Home() {
             <p className="value">Kanfla Cl, Accra</p>
           </div>
         </div>
+        <div className="details-item">
+          <div className="icon-box">
+            <i className="fas fa-star"></i>
+          </div>
+          <div className="details-text">
+            <span className="label">Rating 4.3</span>
+            <div className="star">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star-half-alt"></i>
+            </div>
+          </div>
+        </div>
 
         <div className="details-item">
           <div className="icon-box">
@@ -107,7 +122,7 @@ function Home() {
           <img src="img-3.jpeg" alt="Togetherness" />
           <div className="story-text">
             <h3 className="sub-text">At Crab Boutique,</h3>
-            <p>
+            <p className="text">
               Every table is more than a place to eat — it’s where friendships
               grow, laughter fills the air, and games turn into memories. Each
               crab dish is part of the fun.
@@ -120,7 +135,7 @@ function Home() {
           <img src="img-4.jpeg" alt="Freshness" />
           <div className="story-text">
             <h3 className="sub-text">From the ocean’s depth to your plate,</h3>
-            <p>
+            <p className="text">
               We bring the finest crabs and seafood, prepared with care so every
               bite carries the taste of freshness and authenticity.
             </p>
@@ -132,7 +147,7 @@ function Home() {
           <img src="img-5.jpeg" alt="Artistry" />
           <div className="story-text">
             <h3 className="sub-text">Boutique Creations</h3>
-            <p>
+            <p className="text">
               Every dish is a blend of tradition and bold flavors, plated with
               artistry and served with passion to satisfy your soul.
             </p>
@@ -144,7 +159,7 @@ function Home() {
           <img src="img-6.jpeg" alt="Connection" />
           <div className="story-text">
             <h3 className="sub-text">An experience of connection.</h3>
-            <p>
+            <p className="text">
               At Crab Boutique, meals become stories, and stories become
               memories worth sharing with those you love.
             </p>
